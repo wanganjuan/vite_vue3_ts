e398c85ebde3d423a5184950cb27232f26f05983
wanganjuan <z798716565@Outlook.com>
2024-07-05 16:00:52 +0800

    移除不常用的插件以及包

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -4,6 +4,7 @@ declare module "*.scss" {
 }
 
 declare module "*.vue" {
-  import Vue from "vue"
-  export default Vue
+  import { DefineComponent } from "vue"
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
--- vite.config.ts
+++ vite.config.ts
@@ -64,7 +64,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
           manualChunks: {
             vue: ["vue", "vue-router", "pinia"],
             element: ["element-plus", "@element-plus/icons-vue"],
-            vxe: ["vxe-table", "vxe-table-plugin-element", "xe-utils"]
+     
           }
         }
       }
